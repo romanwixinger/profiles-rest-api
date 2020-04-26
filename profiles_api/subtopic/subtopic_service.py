60c250de06587a7309f13e42453a7a7c191cee53
romanwixinger <roman.wixinger@gmail.com>
2020-04-26 19:33:05 +0200

    Created UserProfileService.

--- profiles_api/subtopic/subtopic_service.py
+++ profiles_api/subtopic/subtopic_service.py
@@ -1,5 +1,5 @@
 from profiles_api.models import UserProfile
-from profiles_api.user_profile_service import get_subtopic_statistics
+from profiles_api.user_profile_service import UserProfileService
 from profiles_api.subtopic.subtopic_model import Subtopic
 
 
@@ -9,7 +9,7 @@ class SubtopicService:
     def get_recommended_subtopics(cls, user: UserProfile, number: int = 2) -> list:
         """Evaluates all completed tests of the user and recommends subtopics accordingly"""
 
-        subtopic_dict = get_subtopic_statistics(user)
+        subtopic_dict = UserProfileService.get_subtopic_statistics(user)
 
         subtopic_list = subtopic_dict.keys()
         ratio_list = [subtopic_dict[x]["ratio"] for x in subtopic_list]
--- profiles_api/user_profile_service.py
+++ profiles_api/user_profile_service.py
@@ -2,57 +2,61 @@ from profiles_api.models import UserProfile
 from profiles_api.completed_test.completed_test_model import CompletedTest
 
 
-def get_subtopic_statistics(user: UserProfile):
-    """Return a  a dict with subtopics as key and dict with statistics as value."""
+class UserProfileService:
+    """Services that are related to the user"""
 
-    subtopic_weight = 1
-    dependency_weight = 1
+    @classmethod
+    def get_subtopic_statistics(cls, user: UserProfile):
+        """Return a dict with subtopics as key and dict with statistics as value."""
 
-    filter_dict = {'user_profile': user.id}
-    completed_tests = CompletedTest.objects.filter(**filter_dict)
-    if completed_tests is None:
-        return None
+        subtopic_weight = 1
+        dependency_weight = 1
 
-    # Create a dict with subtopics as key and dict with relevant information as value.
-    subtopic_dict = {}
+        filter_dict = {'user_profile': user.id}
+        completed_tests = CompletedTest.objects.filter(**filter_dict)
+        if completed_tests is None:
+            return None
 
-    for completed_test in completed_tests:
+        # Create a dict with subtopics as key and dict with relevant information as value.
+        subtopic_dict = {}
 
-        answers = completed_test.answers.all()
+        for completed_test in completed_tests:
 
-        for answer in answers:
-            if str(answer.question.subtopic_id) not in subtopic_dict:
-                subtopic_dict[str(answer.question.subtopic_id)] = {"correct": 0, "incorrect": 0}
+            answers = completed_test.answers.all()
 
-            if answer.correct:
-                subtopic_dict[str(answer.question.subtopic_id)]["correct"] += subtopic_weight
-            else:
-                subtopic_dict[str(answer.question.subtopic_id)]["incorrect"] += subtopic_weight
-
-            if answer.question.dependencies is None or answer.question.dependencies == []:
-                continue
-            for dependency in answer.question.dependencies.all():
-                if str(dependency.id) not in subtopic_dict:
-                    subtopic_dict[str(dependency.id)] = {"correct": 0, "incorrect": 0}
+            for answer in answers:
+                if str(answer.question.subtopic_id) not in subtopic_dict:
+                    subtopic_dict[str(answer.question.subtopic_id)] = {"correct": 0, "incorrect": 0}
 
                 if answer.correct:
-                    subtopic_dict[str(dependency.id)]["correct"] += dependency_weight
+                    subtopic_dict[str(answer.question.subtopic_id)]["correct"] += subtopic_weight
                 else:
-                    subtopic_dict[str(dependency.id)]["incorrect"] += dependency_weight
+                    subtopic_dict[str(answer.question.subtopic_id)]["incorrect"] += subtopic_weight
 
-    for key in subtopic_dict.keys():
-        subtopic_dict[key]["ratio"] = subtopic_dict[key]["correct"] / (subtopic_dict[key]["correct"] + subtopic_dict[key]["incorrect"])
+                if answer.question.dependencies is None or answer.question.dependencies == []:
+                    continue
+                for dependency in answer.question.dependencies.all():
+                    if str(dependency.id) not in subtopic_dict:
+                        subtopic_dict[str(dependency.id)] = {"correct": 0, "incorrect": 0}
 
-    return subtopic_dict
+                    if answer.correct:
+                        subtopic_dict[str(dependency.id)]["correct"] += dependency_weight
+                    else:
+                        subtopic_dict[str(dependency.id)]["incorrect"] += dependency_weight
 
+        for key in subtopic_dict.keys():
+            subtopic_dict[key]["ratio"] = subtopic_dict[key]["correct"] / (subtopic_dict[key]["correct"] + subtopic_dict[key]["incorrect"])
 
-def get_user(user_id: int):
-    """Gets the user object by its id"""
+        return subtopic_dict
 
-    filter_dict = {'id': user_id}
-    user = UserProfile.objects.filter(**filter_dict)[0]
+    @classmethod
+    def get_user(cls, user_id: int):
+        """Gets the user object by its id"""
 
-    return user
+        filter_dict = {'id': user_id}
+        user = UserProfile.objects.filter(**filter_dict)[0]
+
+        return user
 
 
 
