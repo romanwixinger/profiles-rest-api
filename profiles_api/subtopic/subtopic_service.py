ed3ff3bacf9a0f96b377f75033554a1abc40d26a
romanwixinger <roman.wixinger@gmail.com>
2020-05-16 22:35:18 +0200

    Improved readability of the search_subtopics method of the SubtopicService.

--- profiles_api/subtopic/subtopic_service.py
+++ profiles_api/subtopic/subtopic_service.py
@@ -1,9 +1,8 @@
-import random
-
 from profiles_api.models import UserProfile
 from profiles_api.subtopic.subtopic_model import Subtopic
 from profiles_api.completed_test.completed_test_model import CompletedTest
 
+from profiles_api.topic.topic_service import TopicService
 from profiles_api.answer.answer_service import AnswerService
 from profiles_api.knowledge_level.knowledge_level_service import KnowledgeLevelService
 
@@ -27,30 +26,14 @@ class SubtopicService:
     def search_subtopics(cls, query_params_dict: dict) -> [Subtopic]:
         """Get subtopics according to query parameters stored in a dict"""
 
-        topic = query_params_dict['topic'] if 'topic' in query_params_dict else None
-        topic_id = query_params_dict['topic_id'] if 'topic_id' in query_params_dict else None
-        start = query_params_dict['start'] if 'start' in query_params_dict else None
-        number = query_params_dict['number'] if 'number' in query_params_dict else None
-        mode = query_params_dict['mode'] if 'mode' in query_params_dict else None
-
-        if topic is not None:
-            filter_dict = {'topic__name': topic}
-            subtopics = Subtopic.objects.filter(**filter_dict)
-        elif topic_id is not None:
-            filter_dict = {'topic__id': topic_id}
-            subtopics = Subtopic.objects.filter(**filter_dict)
+        if 'topic' in query_params_dict:
+            subtopics = Subtopic.objects.filter(**{'topic__name': query_params_dict['topic']})
+        elif 'topic_id' in query_params_dict:
+            subtopics = Subtopic.objects.filter(**{'topic__id': query_params_dict['topic_id']})
         else:
             subtopics = Subtopic.objects.all()
 
-        subtopics = list(subtopics)
-
-        if mode == 'random':
-            random.shuffle(subtopics)
-        if start is not None:
-            subtopics = subtopics[min(abs(int(start)), len(subtopics)):]
-        if number is not None:
-            subtopics = subtopics[:max(0, min(int(number), len(subtopics)))]
-
+        subtopics = TopicService.select_items(items=list(subtopics), query_params_dict=query_params_dict)
         return subtopics
 
     @classmethod
