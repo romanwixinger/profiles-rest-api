6736174092a57cd78c2bd165446b194e7bb36e40
romanwixinger <roman.wixinger@gmail.com>
2020-08-10 22:52:46 +0200

    Deactivated standard views.

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -105,7 +105,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (permissions.UpdateOwnProfile,)
+    permission_classes = (permissions.UpdateOwnProfile, IsAuthenticated)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
 
